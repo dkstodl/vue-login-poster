fe22f30163d899322ec8939fd2a872116bb517d6
dksgmlwls <dkstodl@naver.com>
2024-10-13 19:06:39 +0900

    first commit

---
+++ vue-til/README.md
@@ -0,0 +1,16 @@
+## 기술 스택
+
+- Vue.js 2.6.10
+- Vue Router
+- Vuex
+- Axios
+
+## 개발 환경
+
+- [Chrome](https://www.google.com/intl/ko/chrome/)
+- [Git](https://git-scm.com/downloads)
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [Node.js LTS 버전(v10.x 이상)](https://nodejs.org/ko/)
+- [Vue.js Dev Tools](https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd)
+
+
---
+++ vue-til/src/components/common/AppFooter.vue
@@ -0,0 +1,30 @@
+<template>
+	<footer>
+		<a href="https://github.com/" target="_blank">View GitHub source</a>
+		<div>© 2024 HEEJIN</div>
+	</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+footer {
+	/* position: fixed;
+	bottom: 0;
+	width: 100%; */
+	height: 52px;
+	padding: 0 20px;
+	margin-top: 50px;
+	font-size: 14px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background: #e5790e;
+	color: white;
+}
+footer a {
+	color: white;
+}
+</style>
